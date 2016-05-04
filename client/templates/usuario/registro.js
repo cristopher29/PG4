f11f761d0fe66607b6c3ce394a1e93ea724ba26f
darkpetrus <pedro.lopez948@gmail.com>
2016-05-04 17:27:17 +0200

    Añadido insertar insertar Usuarios
    *Fix #ids

---
+++ client/templates/usuario/registro.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+Template.registro.events({
+    "click #registro": function(evento)
+    {
+        evento.preventDefault();
+
+        var nombre = jQuery('#nombre').val();
+        var apellido = jQuery('#apellido').val();
+        var telefono = jQuery('#telefono').val();
+        var email = jQuery('#email').val();
+        var password = jQuery('#password').val();
+
+        usuario = {nombre: nombre, apellido: apellido, telefono: telefono, email: email, password: password};
+
+        Meteor.call('crearUsuario', articulo );
+
+    }
+});
